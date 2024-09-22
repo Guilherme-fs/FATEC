dcb61187cb79efd2d418249b8505956d8ba62c89
Guilherme-fs <67802687+Guilherme-fs@users.noreply.github.com>
2024-09-22 12:06:56 -0300

    Add files via upload

---
+++ Sistemas Operacionais I/ThreadsComSemaforos/ControlarSaquesDepositos/src/controlarsaquesdepositos/ControlarSaquesDepositos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package controlarsaquesdepositos;
+
+/**
+ *
+ * @author guilh
+ */
+public class ControlarSaquesDepositos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
